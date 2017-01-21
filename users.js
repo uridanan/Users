cf68bea8e1850d95494631e37bda15aa0dc35ad5
uridanan <uridanan@gmail.com>
2017-01-21 23:21:06 +0200

    Load users and roles locally
    Load data from local json files
    Create model in controller

--- users.js
+++ users.js
@@ -1,6 +1,11 @@
 'use strict';
 
-var app = angular.module('demo', ['ngSanitize', 'ui.select']);
+var app = angular.module('UsersApp', ['ngSanitize', 'ui.select']);
+
+var args = {
+  scope:  null,
+  http: null
+}
 
 /**
  * AngularJS default filter with the following expression:
@@ -39,7 +44,110 @@ app.filter('propsFilter', function() {
   };
 });
 
-app.controller('DemoCtrl', function($scope, $http, $timeout) {
+
+app.controller('UsersCtrl', main);
+
+function main($scope, $http, $timeout){
+  args.scope = $scope;
+  args.http = $http;
+  fetchRoles();
+}
+
+function fetchRoles(){
+  loadJsonFile(args.http, 'http://localhost:8000/roles.json',initRoles);
+  //  $scope.roles = [
+  //    {name: 'GUEST', id: '1'},
+  //    {name: 'ADMIN', id: '2'},
+  //    {name: 'DEV', id: '3'},
+  //    {name: 'QA', id: '4'}
+  //  ];
+}
+
+function initRoles(data){
+  args.scope.roles = [];
+  for  (var i=0 ; i < data.length ; i++){
+    var r = data[i];
+    var dict = {};
+    dict.id = r.id;
+    dict.name = r.name;
+    args.scope.roles.push(dict);
+  }
+
+  //Continue to next methods
+  // initControlButtons(args.scope);
+  // initTagsControl(args.scope);
+  fetchUsers();
+}
+
+function fetchUsers(){
+  loadJsonFile(args.http, 'http://localhost:8000/users.json',initUsers);
+}
+
+function initUsers(data){
+  args.scope.users = [];
+  for  (var i=0 ; i < data.length ; i++){
+    var u = data[i];
+    var dict = {};
+    dict.id = u.id;
+    dict.name = u.name;
+    args.scope.users.push(dict);
+  }
+
+  //Continue to next methods
+  initControlButtons($scope);
+  initTagsControl($scope);
+}
+
+function addUserRow(user){
+  
+}
+
+function initTagsControl($scope){
+  $scope.multipleDemo = {};
+  $scope.multipleDemo.selectedPeople = [$scope.roles[0], $scope.roles[1]];
+
+  $scope.addPerson = function(item, model){
+    // if(item.hasOwnProperty('isTag')) {
+    //   delete item.isTag;
+    //   $scope.roles.push(item);
+    // }
+  }
+
+  $scope.someGroupFn = function (item){
+
+    if (item.name[0] >= 'A' && item.name[0] <= 'M')
+        return 'From A - M';
+
+    if (item.name[0] >= 'N' && item.name[0] <= 'Z')
+        return 'From N - Z';
+
+  };
+
+  $scope.counter = 0;
+  $scope.someFunction = function (item, model){
+    $scope.counter++;
+    $scope.eventResult = {item: item, model: model};
+  };
+
+  $scope.removed = function (item, model) {
+    $scope.lastRemoved = {
+        item: item,
+        model: model
+    };
+  };
+
+  $scope.tagTransform = function (newTag) {
+    var item = {
+        name: newTag,
+        id: "0"
+    };
+
+    return item;
+  };
+
+}
+
+function initControlButtons($scope){
   $scope.disabled = undefined;
   $scope.searchEnabled = undefined;
 
@@ -64,88 +172,30 @@ app.controller('DemoCtrl', function($scope, $http, $timeout) {
     $scope.address.selected = undefined;
     $scope.country.selected = undefined;
   };
+}
 
-  $scope.someGroupFn = function (item){
-
-    if (item.name[0] >= 'A' && item.name[0] <= 'M')
-        return 'From A - M';
-
-    if (item.name[0] >= 'N' && item.name[0] <= 'Z')
-        return 'From N - Z';
-
-  };
-
-
-  $scope.counter = 0;
-  $scope.someFunction = function (item, model){
-    $scope.counter++;
-    $scope.eventResult = {item: item, model: model};
-  };
-
-  $scope.removed = function (item, model) {
-    $scope.lastRemoved = {
-        item: item,
-        model: model
-    };
-  };
-
-  $scope.tagTransform = function (newTag) {
-    var item = {
-        name: newTag,
-        email: newTag.toLowerCase()+'@email.com',
-        age: 'unknown',
-        country: 'unknown'
-    };
-
-    return item;
-  };
-
-  $scope.userRoles = {};
-  $scope.roles = [
-    {displayName: 'GUEST', id: '1'},
-    {displayName: 'ADMIN', id: '2'},
-    {displayName: 'DEV', id: '3'},
-    {displayName: 'QA', id: '4'}
-  ];
-
-
-  $scope.person = {};
-  $scope.people = [
-    { name: 'Adam',      email: 'adam@email.com',      age: 12, country: 'United States' },
-    { name: 'Amalie',    email: 'amalie@email.com',    age: 12, country: 'Argentina' },
-    { name: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina' },
-    { name: 'Adrian',    email: 'adrian@email.com',    age: 21, country: 'Ecuador' },
-    { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30, country: 'Ecuador' },
-    { name: 'Samantha',  email: 'samantha@email.com',  age: 30, country: 'United States' },
-    { name: 'Nicole',    email: 'nicole@email.com',    age: 43, country: 'Colombia' },
-    { name: 'Natasha',   email: 'natasha@email.com',   age: 54, country: 'Ecuador' },
-    { name: 'Michael',   email: 'michael@email.com',   age: 15, country: 'Colombia' },
-    { name: 'Nicolás',   email: 'nicolas@email.com',    age: 43, country: 'Colombia' }
-  ];
-
-  $scope.multipleDemo = {};
-  $scope.multipleDemo.selectedPeople = [$scope.roles[0], $scope.roles[1]];
-
-
-  $scope.address = {};
-  $scope.refreshAddresses = function(address) {
-    var params = {address: address, sensor: false};
-    return $http.get(
-      'http://maps.googleapis.com/maps/api/geocode/json',
-      {params: params}
-    ).then(function(response) {
-      $scope.addresses = response.data.results;
-    });
-  };
-
-  $scope.addPerson = function(item, model){
-    if(item.hasOwnProperty('isTag')) {
-      delete item.isTag;
-      $scope.roles.push(item);
+function loadJsonFile($http, filename, processResponse){
+  console.log("Load data from file: "+filename);
+  $http.get(filename,
+    {
+        cache: false,
+        transformResponse: function (data, headersGetter) {
+            try {
+                var jsonObject = JSON.parse(data); // verify that json is valid
+                return jsonObject;
+            }
+            catch (e) {
+                console.log("did not receive a valid Json: " + e)
+            }
+            return {};
+        }
     }
-  }
-
-});
+  ).then(function(response){
+      var data = response.data;
+      console.log(data);
+      processResponse(data);
+  });
+}
 
 
 //TODO
