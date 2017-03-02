537c0c07e06c5b7845f32ebbdca76e3d1b6c7f59
uridanan <uridanan@gmail.com>
2017-03-02 21:38:28 +0200

    Cleanup
    Move final files to src folder
    Remove redundant files

--- src/app.js
+++ src/app.js
@@ -7,6 +7,21 @@ var global = {
 var app = angular.module('UsersApp', ['ngSanitize', 'ui.select']);
 
 //-----------------------------------------------------------------------------
+// Make sure AngularJS calls our WCF Service as a "GET", rather than as an "OPTION"
+app.config(['$httpProvider', function ($httpProvider) {
+    //$httpProvider.defaults.headers.common = {};
+    //$httpProvider.defaults.headers.post = {};
+    //$httpProvider.defaults.headers.put = {};
+    //$httpProvider.defaults.headers.patch = {};
+    //$httpProvider.defaults.useXDomain = true;
+    //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    //$httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+    //Login info for TT
+    //$httpProvider.defaults.headers.common['Authorization'] = 'Basic YWxtOnR0MTIzNA==';
+}]);
+
+//-----------------------------------------------------------------------------
 //Filter for the UI select elements
 app.filter('propsFilter', getUISelectFilter);
 
